2d8af3ad319e92300391b6d0705a4bb010159e1a
pakchee0529 <pakchee0529@gmail.com>
2026-03-07 14:18:09 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, abort
+import json
+import math
+import os
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, LocationMessage, TextSendMessage
+from linebot.exceptions import InvalidSignatureError
+
+app = Flask(__name__)
+
+CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+with open("coords.json", "r", encoding="utf-8") as f:
+    coords = json.load(f)
+
+def distance(lat1, lon1, lat2, lon2):
+    R = 6371000
+    phi1 = math.radians(lat1)
+    phi2 = math.radians(lat2)
+    dphi = math.radians(lat2-lat1)
+    dlambda = math.radians(lon2-lon1)
+
+    a = math.sin(dphi/2)**2 + math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2
+    return 2*R*math.atan2(math.sqrt(a), math.sqrt(1-a))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location(event):
+
+    user_lat = event.message.latitude
+    user_lon = event.message.longitude
+
+    nearest = None
+    min_dist = 999999999
+
+    for c in coords:
+        d = distance(user_lat, user_lon, c["lat"], c["lon"])
+
+        if d < min_dist:
+            min_dist = d
+            nearest = c
+
+    text = f"最寄り地点\n{nearest['name']}\n距離: {int(min_dist)}m"
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=text)
+    )
+
+
+if __name__ == "__main__":
+    app.run()
